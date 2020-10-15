7e299befde464e5b0505c32061afd1b2c8d90490
James Hrivnak <jameshrivnak4@gmail.com>
2020-10-15 17:06:13 -0600

    for pair

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    alert(array + ' ' + n);
     if (n === undefined) {
       return array[array.length - 1];
     } else if (n === 0) {
