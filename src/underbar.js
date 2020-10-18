aa82d2d96cde32ace5db1258c9d8ee805f3f65d1
James Hrivnak <jameshrivnak4@gmail.com>
2020-10-17 22:03:36 -0600

    Complete Bare Minimum Requirements

--- src/underbar.js
+++ src/underbar.js
@@ -373,7 +373,14 @@
   // input array. For a tip on how to make a copy of an array, see:
   // http://mdn.io/Array.prototype.slice
   _.shuffle = function(array) {
-
+    let result = array.slice();
+    for (let i = result.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = result[i];
+      result[i] = result[j];
+      result[j] = temp;
+    }
+    return result;
   };
 
 
